517fd6d53683006bc9906cac6561e9e430da20b5
Dylan Starink <dylan@starink.com>
2024-02-08 10:52:26 -0800

    Add Joi

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import joi from "joi";
 
 const app = express();
 dotenv.config();
+
+interface EnvVars {
+  PORT: number;
+  HOST: string;
+}
+
+const envSchema = joi.object<EnvVars, true>({
+  PORT: joi.number().positive().default(8000),
+  HOST: joi.string().default("localhost"),
+}).unknown()
+
+// Validate and get typescript type
+const {error, value: envVars} = envSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Env Vars validation error: ${error.message}`);
+}
 
 // So we can see the requests in the console
 app.use(morgan("dev"));
@@ -16,6 +34,6 @@ app.get("/", (req, res) => {
 // start the server
 app.listen(process.env.PORT, () => {
   console.log(
-    `Server running : http://${process.env.HOST}:${process.env.PORT}`,
+    `Server running : http://${envVars.HOST}:${envVars.PORT}`,
   );
 });
