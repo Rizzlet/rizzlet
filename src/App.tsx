5038b0243b159ca47757a8e37b5d7c6d4b3dea33
Dylan Starink <dylan@starink.com>
2024-02-07 20:50:05 -0800

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
 export default function App() {
-  const name = 'world';
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
