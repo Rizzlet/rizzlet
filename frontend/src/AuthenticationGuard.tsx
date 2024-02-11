cc0f43cc644cc5228248ac15cd93059438216edc
Dylan Starink <dylan@starink.com>
2024-02-11 14:52:28 -0800

    Add Auth context and Provider

--- frontend/src/AuthenticationGuard.tsx
+++ frontend/src/AuthenticationGuard.tsx
@@ -1,18 +1,30 @@
-// import { createContext, useContext } from "react";
+import {
+  createSearchParams,
+  redirect,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import { useAuth } from "./AuthContext";
+import { useEffect } from "react";
 
-// const AuthContext = createContext<null | any>(localStorage.getItem("token"));
+export function AuthGuard({ children }: { children: React.ReactNode }) {
+  const auth = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-// export const useAuthContext = () => {
-//   const context = useContext(AuthContext);
-//   if (!context) {
-//     throw new Error("useAuthContext must be used within an AuthProvider");
-//   }
-//   return context;
-// };
+  useEffect(() => {
+    if (!auth.isLoggedIn) {
+      navigate(
+        {
+          pathname: "/login",
+          search: createSearchParams({
+            from: location.pathname,
+          }).toString(),
+        },
+        { replace: true }
+      );
+    }
+  }, [auth.isLoggedIn, navigate, location]);
 
-// // AUth stored as a JWT in a cookie
-// const AuthProvider = ({ children }) => {
-//   return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
-// };
-
-export {};
+  return <>{children}</>;
+}
