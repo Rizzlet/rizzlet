5297f38d9205ddce17fe60ada29c47a2df9c01f7
Kevin <kev.yuan01@gmail.com>
2024-02-12 15:52:01 -0800

    Added function to when a flashcard is clicked

--- frontend/src/Components/flashcards.tsx
+++ frontend/src/Components/flashcards.tsx
@@ -1,10 +1,19 @@
-const flashcard: Function  = (props: any) => {
+export default function Flashcard(props: any){
+
+  /**
+ * @param question: the question that the user writes
+ */
+
+  function selectedQuestion(){
+    const retrieved_flashcard: HTMLElement | null = document.getElementById("test");
+    if (retrieved_flashcard){
+      retrieved_flashcard.style.backgroundColor = "red";
+    }
+  }
 
   return(
-    <div>
-
+    <div id="test" onClick = {selectedQuestion} className = "bg-pink-400 rounded-md w-1/4 hover:shadow-xl cursor-pointer">
+      <h1 className = "text-center relative">lorem ipsum</h1>
     </div>
   );
 }
-
-export default flashcard;
