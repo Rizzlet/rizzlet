ce18b6a1814c0fa1121fdd21caa3a1deb3772ba9
Kevin <kev.yuan01@gmail.com>
2024-02-13 00:36:07 -0800

    Dynamically changed styling when you click on the flashcard

--- frontend/src/Components/flashcards.tsx
+++ frontend/src/Components/flashcards.tsx
@@ -1,18 +1,22 @@
+import { useState } from "react";
+
 export default function Flashcard(props: any){
 
   /**
  * @param question: the question that the user writes
  */
-
-  function selectedQuestion(){
-    const retrieved_flashcard: HTMLElement | null = document.getElementById("test");
-    if (retrieved_flashcard){
-      retrieved_flashcard.style.backgroundColor = "red";
-    }
+  
+  // Because of how tailwind works, styles have to be predefined so that it can be dynamically changed
+  const enum selectedVariants {
+    selected = "bg-pink-400 rounded-md w-1/4 hover:shadow-xl cursor-pointer",
+    notSelected = "bg-black rounded-md w-1/4 hover:shadow-xl cursor-pointer"
   }
 
+  // Checks to see if the user has selected this question or not
+  let [selected, setSelected] = useState(false);
+
   return(
-    <div id="test" onClick = {selectedQuestion} className = "bg-pink-400 rounded-md w-1/4 hover:shadow-xl cursor-pointer">
+    <div id="test" onClick = {() => {setSelected(!selected)}} className = {`${selected ? selectedVariants.selected : selectedVariants.notSelected}`}>
       <h1 className = "text-center relative">lorem ipsum</h1>
     </div>
   );
