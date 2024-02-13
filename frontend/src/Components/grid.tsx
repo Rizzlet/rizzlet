63e0f8433b4d119cb950ba6536e6210d141631da
Kevin <kev.yuan01@gmail.com>
2024-02-13 15:21:07 -0800

    Created grid to load questions

--- frontend/src/Components/grid.tsx
+++ frontend/src/Components/grid.tsx
@@ -1,3 +1,10 @@
-export default function questionGrid(){
-  
+import Flashcard from "./flashcards";
+
+function CreateGrid(){
+  let testArray: String[] = ["lorem", "ipsum", "questionlol", "yep"];
+  return <>{testArray.map(x => {return <Flashcard question={x} type="tf"></Flashcard>})}</>;
+}
+
+export default function QuestionGrid(){
+  return <CreateGrid></CreateGrid>;
 }
