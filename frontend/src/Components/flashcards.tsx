a4c7a1ce1691b5162d9c955fada81da996e9e686
Kevin <kev.yuan01@gmail.com>
2024-02-13 15:20:46 -0800

    Added answers component in flashcard

--- frontend/src/Components/flashcards.tsx
+++ frontend/src/Components/flashcards.tsx
@@ -1,23 +1,26 @@
 import { useState } from "react";
+import Answers from "./answers";
 
 export default function Flashcard(props: any){
 
   /**
  * @param question: the question that the user writes
+ * @param type: passed down to answers component
  */
   
   // Because of how tailwind works, styles have to be predefined so that it can be dynamically changed
-  const enum selectedVariants {
-    selected = "bg-pink-400 rounded-md w-1/4 hover:shadow-xl cursor-pointer",
-    notSelected = "bg-black rounded-md w-1/4 hover:shadow-xl cursor-pointer"
+  const enum selectedVariants{
+    selected = "bg-pink-400 rounded-md size-32 hover:shadow-xl cursor-pointer m-8 sticky",
+    notSelected = "bg-red-50 rounded-md size-24 hover:shadow-xl cursor-pointer m-8 relative"
   }
 
   // Checks to see if the user has selected this question or not
   let [selected, setSelected] = useState(false);
 
   return(
-    <div id="test" onClick = {() => {setSelected(!selected)}} className = {`${selected ? selectedVariants.selected : selectedVariants.notSelected}`}>
-      <h1 className = "text-center relative">lorem ipsum</h1>
+    <div onClick = {() => {setSelected(!selected)}} className = {`${selected ? selectedVariants.selected : selectedVariants.notSelected}`}>
+      <h1 className = "text-center">{props.question}</h1>
+      <Answers type={props.type} hidden={`${selected ? "visible" : "hidden"}`} answerOptions={[{answer: "lol"}, {answer: "ahhhhhhhhh"}, {answer: "touhou"}]}></Answers>
     </div>
   );
 }
