b243bffaa1831768e845023a669b4fefd0838d74
Kevin <kev.yuan01@gmail.com>
2024-02-13 15:21:14 -0800

    Renders the grid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import QuestionGrid from "./Components/grid";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold bg-black">Hello world!</h1>;
+  return <QuestionGrid></QuestionGrid>;
 }
