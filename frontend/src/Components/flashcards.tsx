258f1dfabafb189f7c7031f793f92d09f2fb2628
Kevin <kev.yuan01@gmail.com>
2024-02-16 10:58:46 -0800

    Changed formatting for questions and added animations

--- frontend/src/Components/flashcards.tsx
+++ frontend/src/Components/flashcards.tsx
@@ -1,26 +1,42 @@
 import { useState } from "react";
 import Answers from "./answers";
+import { currentCardPosition } from "./questionRender";
 
 export default function Flashcard(props: any){
 
   /**
  * @param question: the question that the user writes
  * @param type: passed down to answers component
+ * @param position: where it is on screen
  */
   
   // Because of how tailwind works, styles have to be predefined so that it can be dynamically changed
   const enum selectedVariants{
-    selected = "bg-pink-400 rounded-md size-32 hover:shadow-xl cursor-pointer m-8 sticky",
-    notSelected = "bg-red-50 rounded-md size-24 hover:shadow-xl cursor-pointer m-8 relative"
+    selected = "bg-pink-400 rounded-lg size-2/5 shadow-2xl cursor-pointer absolute",
+    notSelected = "bg-red-50 rounded-lg size-2/5 shadow-2xl cursor-pointer absolute",
+  
+  }
+
+  const enum questionFormattingVariants{
+    selected = "flex justify-center items-center h-1/6 animate-question-up",
+    notSelected = "flex justify-center items-center h-full animate-question-down"
+  }
+
+  function checkPosition(){
+    if (props.position === currentCardPosition){
+      setSelected(!selected);
+    }
   }
 
   // Checks to see if the user has selected this question or not
   let [selected, setSelected] = useState(false);
 
   return(
-    <div onClick = {() => {setSelected(!selected)}} className = {`${selected ? selectedVariants.selected : selectedVariants.notSelected}`}>
-      <h1 className = "text-center">{props.question}</h1>
-      <Answers type={props.type} hidden={`${selected ? "visible" : "hidden"}`} answerOptions={[{answer: "lol"}, {answer: "ahhhhhhhhh"}, {answer: "touhou"}]}></Answers>
+    <div onClick = {() => {checkPosition()}} className = {`${selected ? selectedVariants.selected : selectedVariants.notSelected} -translate-y-${props.position * 10} translate-x-${props.position * 10}`}>
+      <div className = {`${selected ? questionFormattingVariants.selected : questionFormattingVariants.notSelected}`}>
+        <h1>{props.question}</h1>
+      </div>
+      <Answers type={props.type} hidden={`${selected ? "visible" : "hidden"}`} answerOptions={[{answer: "lol"}, {answer: "ahhhhhhhhh"}, {answer: "touhou"}, {answer: "lol"}]}></Answers>
     </div>
   );
 }
