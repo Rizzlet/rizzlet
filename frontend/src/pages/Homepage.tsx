93697c9456e7414ab81da185df7ecacae9720eeb
nrojo1124 <naomi.ouat.1124@gmail.com>
2024-02-23 15:17:40 -0800

    more stuff

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -17,14 +17,19 @@ const FrontPage: React.FC = () => {
           </div>
         </div>
         <nav className="justify-self-end flex-1 min-w-0 space-x-4">
-          <Link
-            className="text-sm font-medium hover:underline underline-offset-4"
-            to="/login"
-          >
-            Login
-          </Link>
+          <Link className="text-sm font-medium hover:underline underline-offset-4" to="/login">Login/Sign Up</Link>
+          <Link className="text-sm font-medium hover:underline underline-offset-4" to="/">About</Link>
         </nav>
       </header>
+      <main className="flex-1">
+        <section className="w-full py-12 md:py-24 lg:py-32 border-t">
+          <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-10">
+            <div className="space-y-3">
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Ace your exams with Rizzlet's interactive flashcards!</h2>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
