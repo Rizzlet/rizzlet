5be0c29131bcc1c0cf766f7281ad159a9681889b
Dylan Starink <dylan@starink.com>
2024-02-23 16:16:13 -0800

    Add NavBar template from online

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,10 +1,12 @@
 import FlashcardField from "./AnswerQuestion";
 import React from "react";
 import QuestionsList from "../components/QuestionsList";
+import NavBar from "../components/Navbar";
 
 export default function App() {
   return (
     <div>
+      <NavBar />
       <h1 className="text-3xl font-bold bg-green-700">Rizzlet!</h1>
     </div>
   );
