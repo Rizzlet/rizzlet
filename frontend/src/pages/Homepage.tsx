f975a79d4917cd6cbd16c5309a99555fab2cbd2a
nrojo1124 <naomi.ouat.1124@gmail.com>
2024-02-26 15:57:46 -0800

    add background to first section

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -5,7 +5,7 @@ const HomePage: React.FC = () => {
   return (
     <div className="flex flex-col min-h-[100dvh]">
       <main className="flex-1">
-        <section className="w-full py-12 md:py-24 lg:py-32">
+        <section className="w-full py-12 md:py-24 lg:py-32 bg-no-repeat bg-cover" style={{backgroundImage : 'url("Online Tools.png")',}}>
           <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-10">
             <div className="space-y-3">
               <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
