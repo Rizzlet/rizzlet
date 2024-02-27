11b50047d3c871f3be5c00c60535e3010276d109
Dylan Starink <dylan@starink.com>
2024-02-26 16:01:51 -0800

    Use backend

--- frontend/src/components/Profilebar.tsx
+++ frontend/src/components/Profilebar.tsx
@@ -1,22 +1,24 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-
-interface IUser{
-  email: string,
-  firstName: string,
-  lastName: string,
-  score: number
+interface IUser {
+  email: string;
+  firstName: string;
+  lastName: string;
+  score: number;
 }
 
-export default function Profilebar(){
-  
-  async function GetUser(): Promise<IUser | null>{
-    try{
-      const response = await axios.get("http://127.0.0.1:8000/api/user", {withCredentials: true});
+export default function Profilebar() {
+  async function GetUser(): Promise<IUser | null> {
+    try {
+      const response = await axios.get(
+        new URL("/api/user", process.env.REACT_APP_BACKEND_URL!).href,
+        {
+          withCredentials: true,
+        }
+      );
       return response.data;
-    }
-    catch(error){
+    } catch (error) {
       console.log("error fetching specific user", error);
       return null;
     }
@@ -25,16 +27,15 @@ export default function Profilebar(){
   const [TheUser, setTheUser] = useState<IUser>();
 
   useEffect(() => {
-    GetUser().then(result => {
-      if (result){
+    GetUser().then((result) => {
+      if (result) {
         setTheUser(result);
-      }
-      else{
+      } else {
         console.log("Error loading questions");
       }
-    })
-  }, [])
-  
+    });
+  }, []);
+
   return (
     <div className="flex justify-items-end bg-white h-7">{TheUser?.score}</div>
   );
