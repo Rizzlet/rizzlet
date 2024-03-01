7a2b95512686e580c8a2a4d3e96f6343b0116d20
Kevin <kev.yuan01@gmail.com>
2024-02-29 16:14:42 -0800

    Passed questionid to associated answers

--- frontend/src/components/Answers.tsx
+++ frontend/src/components/Answers.tsx
@@ -4,6 +4,7 @@ interface IAnswer {
   answerText: string;
   rightAnswer: string;
   alreadyAnswered: boolean;
+  questionAssociated: string;
 }
 
 export default function Answers<T extends IAnswer>(props: T) {
@@ -11,7 +12,18 @@ export default function Answers<T extends IAnswer>(props: T) {
    * @param answerText: Self-Explanatory
    * @param rightAnswer: Indicates whether this specific answer is the right answer.
    * @param alreadyAnswered: whether the question was previously answered. will format based on whether its answered
+   * @param questionAssociated: the question these answers belong to
    */
+
+  // Saves the user's answered question so it can't be answered multiple times by the same user
+  async function handleOnceAnswered() {
+    await axios.post(
+      new URL("/api/answeredquestions", process.env.REACT_APP_BACKEND_URL!)
+        .href,
+      { Question: props.questionAssociated },
+      { withCredentials: true }
+    );
+  }
 
   const enum AnswersVariant {
     correct = "relative h-1/4 w-2/5 bg-green-300 flex items-center justify-around rounded-lg",
@@ -42,10 +54,12 @@ export default function Answers<T extends IAnswer>(props: T) {
     <button
       className={`${props.alreadyAnswered && props.rightAnswer === "true" ? AnswersVariant.correct : props.alreadyAnswered ? AnswersVariant.incorrect : "relative hover: cursor-pointer h-1/4 w-2/5 bg-white flex items-center justify-around rounded-lg"}`}
       onClick={() => {
-        props.rightAnswer === "true" && !props.alreadyAnswered
-          ? updateScore()
-          : console.log("aw");
+        if (props.rightAnswer === "true") {
+          updateScore();
+        }
+        handleOnceAnswered();
       }}
+      disabled={props.alreadyAnswered}
     >
       <h1>{props.answerText}</h1>
     </button>
