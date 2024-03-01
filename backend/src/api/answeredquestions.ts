707e70ce002cb0f751a858108ea167ae9b21809e
Kevin <kev.yuan01@gmail.com>
2024-02-29 16:14:26 -0800

    Handled submitting answered questions to database and altered checking for already answered questions

--- backend/src/api/answeredquestions.ts
+++ backend/src/api/answeredquestions.ts
@@ -3,16 +3,17 @@ import { verifyAndDecodeToken } from "./auth/sharedAuth.js";
 import { answeredquestion } from "../models/answeredquestion.js";
 
 export async function CheckAnswered(req: Request, res: Response) {
-  const { id } = req.body;
   const userData = verifyAndDecodeToken(req.cookies.token);
-
+  const { questionId } = req.body;
   if (!userData) {
     console.log("checking answers authorization failed");
     return;
   }
 
   try {
-    const foundAnsweredQuestion = await answeredquestion.findById(id).exec();
+    const foundAnsweredQuestion = await answeredquestion
+      .findOne({ User: userData.id, Question: questionId })
+      .exec();
     if (foundAnsweredQuestion == null) {
       res.send(false).status(201);
     } else {
@@ -23,4 +24,23 @@ export async function CheckAnswered(req: Request, res: Response) {
   }
 }
 
-export async function SubmitAnsweredQuestion() {}
+export async function SubmitAnsweredQuestion(req: Request, res: Response) {
+  const userData = verifyAndDecodeToken(req.cookies.token);
+
+  if (!userData) {
+    res.status(401);
+    return;
+  }
+
+  try {
+    const newAnsweredQuestion = await new answeredquestion({
+      User: userData.id,
+      Question: req.body.Question,
+    });
+    await newAnsweredQuestion.save();
+    console.log("post successful");
+    res.status(201);
+  } catch (error) {
+    res.status(error.message);
+  }
+}
