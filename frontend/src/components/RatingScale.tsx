bdbff9a347ad7607dad3ae0ada8bedf8669f067a
Dylan Starink <dylan@starink.com>
2024-03-01 17:53:19 -0800

    Add rating scale Questions and scale

--- frontend/src/components/RatingScale.tsx
+++ frontend/src/components/RatingScale.tsx
@@ -3,25 +3,58 @@ import Likert from "react-likert-scale";
 
 export default function RatingScale() {
   return (
-    <Likert
-      responses={[
-        {
-          value: 1,
-          text: "Very Unsatisfied",
-        },
-        {
-          value: 1,
-          text: "Unsatisfied",
-        },
-        {
-          value: 1,
-          text: "Satisfied",
-        },
-        {
-          value: 1,
-          text: "Very Satisfied",
-        },
-      ]}
-    />
+    <>
+      <p>How easy or difficult was this question?</p>
+      <Likert
+        responses={[
+          {
+            value: 1,
+            text: "Very Easy",
+          },
+          {
+            value: 1,
+            text: "Easy",
+          },
+          {
+            value: 1,
+            text: "Neutral",
+          },
+          {
+            value: 1,
+            text: "Difficult",
+          },
+          {
+            value: 1,
+            text: "Very Difficult",
+          },
+        ]}
+      />
+      <p>How relevant was this question to the learning objectives of this class?</p>
+      <Likert
+        responses={[
+          {
+            value: 1,
+            text: "Not relevant at all",
+          },
+          {
+            value: 1,
+            text: "Somewhat relevant",
+          },
+          {
+            value: 1,
+            text: "Moderately relevant",
+          },
+          {
+            value: 1,
+            text: "Very relevant",
+          },
+          {
+            value: 1,
+            text: "Extremely relevant",
+          },
+        ]}
+      />
+
+    </>
   );
 }
