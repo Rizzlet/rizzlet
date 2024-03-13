d667dd6d6f4ba1aa9825b6d51ef1522ecc6ca76e
Kevin Yuan <kev.yuan01@gmail.com>
2024-03-12 22:50:49 -0700

    Change formating

--- frontend/src/pages/AnswerQuestion.tsx
+++ frontend/src/pages/AnswerQuestion.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Flashcard from "../components/Flashcard";
 import axios from "axios";
 import AnswersField from "../components/AnswersField";
@@ -12,13 +12,14 @@ interface Question {
   answer: boolean;
 }
 
-
 // Get questions and answers based on class
-async function fetchQuestions(classId: string | undefined): Promise<Question[]> {
-  
+async function fetchQuestions(
+  classId: string | undefined
+): Promise<Question[]> {
   try {
     const response = await axios.get(
-      new URL(`/api/class/${classId}`, process.env.REACT_APP_BACKEND_URL!).href, {withCredentials:true}
+      new URL(`/api/class/${classId}`, process.env.REACT_APP_BACKEND_URL!).href,
+      { withCredentials: true }
     );
     return response.data;
   } catch (error) {
@@ -31,7 +32,7 @@ export default function FlashcardField() {
   // Used to determine what flashcards is shown on screen. Represents the index of the array of flashcards
   let [questionToRender, changeQuestionToRender] = useState(0);
 
-  // The list of questions in database
+  // The list of questions for a specific class
   let [listOfQuestions, setListofQuestions] = useState<Question[]>([]);
 
   let animationDirection = useRef("none");
@@ -66,53 +67,53 @@ export default function FlashcardField() {
         console.log("Error loading questions");
       }
     });
-  }, []);
+  }, [id]);
 
   return (
-    <div className="relative flex justify-center h-screen w-screen items-center bg-white m-0 p-0">
-      <div className="relative h-3/5 w-3/5 flex-col justify-center items-center m-0 p-0">
-        <div className="relative flex justify-center items-center text-4xl h-full w-full">
+    <div className="relative flex flex-row justify-center h-svh w-dvw items-center bg-white m-0 p-0">
+      <div className="relative h-4/5 w-2/5 flex flex-col justify-center items-center m-0 p-0">
+        <div className="relative flex flex-row justify-center items-center text-4xl h-full w-full">
           {mapQuestions(listOfQuestions)[questionToRender]}
+
+          <button
+            className="absolute h-1/6 w-1/6 left-full"
+            onClick={() => {
+              if (questionToRender === listOfQuestions.length) {
+                changeQuestionToRender(0);
+              } else {
+                changeQuestionToRender(questionToRender++);
+              }
+              animationDirection.current = "left";
+              Sleep(500).then(() => {
+                animationDirection.current = "none";
+              });
+            }}
+          >
+            <h1>right</h1>
+          </button>
+
+          <button
+            className="absolute h-1/6 w-1/6 right-full"
+            onClick={() => {
+              if (questionToRender === -1) {
+                changeQuestionToRender(listOfQuestions.length - 1);
+              } else {
+                changeQuestionToRender(questionToRender--);
+              }
+              animationDirection.current = "right";
+              Sleep(500).then(() => {
+                animationDirection.current = "none";
+              });
+            }}
+          >
+            <h1>left</h1>
+          </button>
         </div>
 
         <AnswersField
           questionlist={listOfQuestions}
           questionToRender={questionToRender}
         ></AnswersField>
-
-        <button
-          className="absolute left-full h-10 w-10"
-          onClick={() => {
-            if (questionToRender === listOfQuestions.length) {
-              changeQuestionToRender(0);
-            } else {
-              changeQuestionToRender(questionToRender++);
-            }
-            animationDirection.current = "left";
-            Sleep(500).then(() => {
-              animationDirection.current = "none";
-            });
-          }}
-        >
-          <h1>right</h1>
-        </button>
-
-        <button
-          className="absolute right-full h-10 w-10"
-          onClick={() => {
-            if (questionToRender === -1) {
-              changeQuestionToRender(listOfQuestions.length - 1);
-            } else {
-              changeQuestionToRender(questionToRender--);
-            }
-            animationDirection.current = "right";
-            Sleep(500).then(() => {
-              animationDirection.current = "none";
-            });
-          }}
-        >
-          <h1>left</h1>
-        </button>
       </div>
     </div>
   );
