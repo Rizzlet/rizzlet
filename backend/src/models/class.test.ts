cbd18cc2664016f107c22eface46fefe9bc84e94
Dylan Starink <dylan@starink.com>
2024-03-15 14:41:55 -0700

    Create class test

--- backend/src/models/class.test.ts
+++ backend/src/models/class.test.ts
@@ -1,10 +1,16 @@
-import { recordHello } from "./hello";
+import { newClass, getClassNames } from "./class";
 import { jest } from "@jest/globals";
 
 beforeEach(() => {
   jest.useFakeTimers();
 });
 
-test("passed", () => {
-  expect(recordHello("test"));
+test("create a class", () => {
+  const run = async () => {
+    expect(newClass("test"));
+    expect((await getClassNames()).length).toBe(1);
+    expect((await getClassNames())[0]).toBe("test");
+  };
+
+  run();
 });
