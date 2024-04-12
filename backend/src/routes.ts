98edae1e29bf54fc8141c7b5890bead5a1d3228e
Kevin Yuan <kev.yuan01@gmail.com>
2024-04-12 02:27:32 -0700

    Created route to get all multiple choice answelrs

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -29,6 +29,7 @@ import { CheckAnswered } from "./api/answeredQuestion.js";
 import { SubmitAnsweredQuestion } from "./api/answeredQuestion.js";
 import { calculateStreak } from "./models/user.js";
 import { getScore } from "./api/users.js";
+import { fetchMultipleChoiceAnswers } from "./api/answers.js";
 
 export function addRoutes(app: Application) {
   app.post("/api/hello", requireAuth, helloWorldHandler);
@@ -55,4 +56,9 @@ export function addRoutes(app: Application) {
   app.put("/api/answeredquestions", requireAuth, CheckAnswered); // Used to check whether a question was already answered
   app.post("/api/answeredquestions", requireAuth, SubmitAnsweredQuestion);
   app.put("/api/user", requireAuth, updateUserClassesHandler);
+  app.get(
+    "/api/question/multipleChoiceAnswers",
+    requireAuth,
+    fetchMultipleChoiceAnswers,
+  );
 }
