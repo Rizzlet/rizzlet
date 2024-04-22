5543c13d3d8f3de1b8383e467cbd461c0c59d699
Anamooz <pthynktwice@gmail.com>
2024-04-22 13:41:09 -0700

    Fixed index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,79 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 
+import { AuthProvider } from "./context/auth/AuthContext";
+import { AuthGuard } from "./context/auth/AuthenticationGuard";
 
+import MyClasses from "./pages/MyClasses";
+import QuestionSubmission from "./pages/FormSubmitQuestions";
+import Login from "./pages/Login";
+import FlashcardField from "./pages/AnswerQuestion";
+import NavBar from "./components/Navbar";
+import HomePage from "./pages/Homepage";
+import QuestionOverview from "./pages/QuestionOverview";
+import ClassDashboard from "./pages/ClassDashboard";
+import ProfilePage from "./pages/ProfilePage";
+
+import "./index.css";
+import RatingPage from "./pages/RatingPage";
+import LeaderBoard from "./pages/LeaderBoardPage";
+import NoClasses from "./pages/AnswerQuestionNoClasses";
+import HealthBarPage from "./components/game/HealthBarPage";
+import PeoplePicker from "./components/game/PeoplePickerPage";
+import TimerPage from "./components/game/TimerPage";
+
+const router = createBrowserRouter([
+  {
+    element: <NavbarWrapper />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/myclasses",
+        element: (
+          <AuthGuard>
+            <MyClasses />
+          </AuthGuard>
+        ),
+      },
+      {
+        path: "/submitQuestion",
+        element: (
+          <AuthGuard>
+            <QuestionSubmission />
+          </AuthGuard>
+        ),
+      },
+      {
+        children: [
+          {
+            path: "/classDashboard/:id",
+            element: (
+              <AuthGuard>
+                <ClassDashboard />
+              </AuthGuard>
+            ),
+          },
+        ],
+      },
+      {
+        path: "/answerQuestions",
+        element: (
+          <AuthGuard>
+            <NoClasses />
+          </AuthGuard>
+        ),
+      },
+      {
+        path: "/answerQuestions/:id",
+        element: (
+          <AuthGuard>
+            <FlashcardField />
+          </AuthGuard>
         ),
       },
       {
@@ -33,17 +107,13 @@
     element: <Login />,
   },
   {
-    path: "/questions/:id",
-    element: < />,
-  }
- {
-  path: "/healthBar",
-  element: <HealthBarPage />,
- },
- {
+    path: "/healthBar",
+    element: <HealthBarPage />,
+  },
+  {
   path: "/timer",
   element: <TimerPage />,
- },
+  },
   {
     path: "/peoplePicker",
     element: (
@@ -66,7 +136,6 @@ root.render(
     </AuthProvider>
   </React.StrictMode>
 );
-
 function NavbarWrapper() {
   return (
     <div>
