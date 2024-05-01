0a1f0132030b4d79657d099efe2b7ca410ebaf4c
Dylan Starink <dylan@starink.com>
2024-05-01 11:53:05 -0700

    Add attacking stage and center Time Elapsed

--- frontend/src/components/game/gamePage.tsx
+++ frontend/src/components/game/gamePage.tsx
@@ -4,10 +4,12 @@ import { AutoFlashcard } from "./AutoFlashcard";
 import Select from "./PeoplePicker";
 import { Timer } from "./Timer";
 
+const NUMBER_OF_QUESTIONS = 5;
+
 interface Question {
   id: string;
   question: string;
-  answer: string;
+  answers: string[];
   possibleAnswers: string[]; // We might want to change this to a IMultipleChoiceAnswers list so that we can house the answer object
 }
 
@@ -18,11 +20,18 @@ interface IMultipleChoiceAnswers {
   question: string;
 }
 
-async function fetchQuestionsAndAnswers(
-  classId: string | undefined
-): Promise<Question[]> {
+interface QuestionResponse {
+  _id: string;
+  class: string;
+  createdBy: string;
+  isHidden: boolean;
+  question: string;
+  type: string;
+}
+
+async function fetchQuestionsAndAnswers(classId: string | undefined) {
   try {
-    const questionResponse = await axios.get<any>(
+    const questionResponse = await axios.get<QuestionResponse[]>(
       new URL(`/api/class/${classId}`, process.env.REACT_APP_BACKEND_URL!).href,
       { withCredentials: true }
     );
@@ -35,7 +44,35 @@ async function fetchQuestionsAndAnswers(
       { withCredentials: true }
     );
 
-    const questions: Question[] = questionResponse.data.map((question: any) => {
+    let unmappedQuestionSet = [] as QuestionResponse[];
+
+    if (questionResponse.data.length === 0) {
+      return "never";
+    }
+
+    if (NUMBER_OF_QUESTIONS > questionResponse.data.length) {
+      while (unmappedQuestionSet.length < NUMBER_OF_QUESTIONS) {
+        console.log(unmappedQuestionSet);
+        console.log("hi");
+        unmappedQuestionSet = unmappedQuestionSet.concat(
+          questionResponse.data
+            .sort(() => Math.random() - Math.random())
+            .slice(
+              0,
+              Math.min(
+                NUMBER_OF_QUESTIONS - unmappedQuestionSet.length,
+                questionResponse.data.length
+              )
+            )
+        );
+      }
+    } else {
+      unmappedQuestionSet = questionResponse.data
+        .sort(() => Math.random() - Math.random())
+        .slice(0, NUMBER_OF_QUESTIONS);
+    }
+
+    const questions: Question[] = unmappedQuestionSet.map((question) => {
       const mappedAnswers = [];
       for (let i = 0; i < answerResponse.data.length; i++) {
         if (question._id === answerResponse.data[i].question) {
@@ -43,10 +80,14 @@ async function fetchQuestionsAndAnswers(
           mappedAnswers.push(answerResponse.data[i].answer);
         }
       }
-      // const answer = answerResponse.data.find((answer: IMultipleChoiceAnswers) => answer.question === question.id && answer.correct === question.answer);
+
+      const answers = answerResponse.data
+        .filter((answer) => answer.question === question._id && answer.correct)
+        .map((a) => a.answer);
       return {
         id: question._id,
         question: question.question,
+        answers: answers,
         possibleAnswers: mappedAnswers,
       };
     });
@@ -65,17 +106,20 @@ enum GameState {
   SelectTarget,
 }
 
-const GamePage: React.FC<GamePageProps> = () => {
-  const [questionSet, setQuestionSet] = useState<Question[] | null>(null);
+export default function GamePage(props: GamePageProps) {
+  const [questionSet, setQuestionSet] = useState<Question[] | null | "never">(
+    null
+  );
   const [selectedPerson, setSelectedPerson] = useState<string | null>(null);
   const [usersInClass, setUsersInClass] = useState<any[]>([]);
   const [start, setStart] = useState(false);
   const [reset, setReset] = useState(false);
   const [timeInCentiseconds, setTimeInCentiseconds] = useState(0);
-  const [notStarted, setNotStarted] = useState(true);
+  const [doingFlashcard, setDoingFlashcard] = useState(false);
+  const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0);
+  const [isAttacking, setIsAttacking] = useState(false);
 
-  const classId = "65d679f08f3afb1b89eebfc3";
-  const disabled = false;
+  const classId = "65d679f68f3afb1b89eebfc5";
 
   useEffect(() => {
     fetchQuestionsAndAnswers(classId).then((questions) => {
@@ -87,7 +131,7 @@ const GamePage: React.FC<GamePageProps> = () => {
   async function fetchUserByClass() {
     try {
       const response = await axios.get<any>(
-        `${process.env.REACT_APP_BACKEND_URL}/api/class/65d679f08f3afb1b89eebfc3/user`,
+        `${process.env.REACT_APP_BACKEND_URL}/api/class/65d679f68f3afb1b89eebfc5/user`,
         {
           withCredentials: true,
         }
@@ -107,7 +151,7 @@ const GamePage: React.FC<GamePageProps> = () => {
   }
 
   function handleSelectPerson(id: string) {
-    if (!disabled) {
+    if (isAttacking) {
       setSelectedPerson(id);
     }
     console.log("Selected person:", id);
@@ -157,18 +201,23 @@ const GamePage: React.FC<GamePageProps> = () => {
         <Select
           selectedPerson={selectedPerson}
           onSelectPerson={handleSelectPerson}
-          disabled={disabled}
+          disabled={!isAttacking}
           people={usersInClass}
         />
         {/* Attack Button */}
         <div className="flex justify-center items-center">
           <button
             type="button"
-            className="flex items-center focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+            className={
+              "flex items-center focus:outline-none text-white  focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2.5 me-2 mb-2 " +
+              (isAttacking && selectedPerson
+                ? "bg-red-700 hover:bg-red-800"
+                : "bg-gray-400")
+            }
             onClick={() =>
               selectedPerson !== null && updateHealth(-2, selectedPerson)
             }
-            disabled={disabled}
+            disabled={!isAttacking}
           >
             {/* Fire Icon */}
             <svg
@@ -191,61 +240,61 @@ const GamePage: React.FC<GamePageProps> = () => {
       </div>
       {/* Right side of the screen */}
 
-      <div className="col-span-1 bg-gray-200 p-4 flex">
+      <div className="col-span-1 bg-gray-200 p-4 flex flex-col content-center">
         {/* TimerPage and AutoFlashcard components */}
-        {notStarted && (
+        {!doingFlashcard && !isAttacking && (
           <div className="container py-10 px-10 min-w-full flex flex-col items-center justify-center">
             <button
-              className="bg-green-600 text-white hover:bg-green-500 font-bold py-2 px-4 mt-3 w-2/3 h-20 rounded"
-              onClick={() => setNotStarted(false)}
+              className={
+                " text-white font-bold py-2 px-4 mt-3 w-2/3 h-20 rounded " +
+                (questionSet === null || questionSet === "never"
+                  ? "bg-gray-300"
+                  : "bg-green-600 hover:bg-green-500")
+              }
+              disabled={questionSet === null || questionSet === "never"}
+              onClick={() => setDoingFlashcard(true)}
             >
-              Start Round!
+              {questionSet === null && "Loading..."}
+
+              {questionSet === "never" && "No Questions Yet..."}
+
+              {questionSet !== null &&
+                questionSet !== "never" &&
+                "Start Round!"}
             </button>
           </div>
         )}
-        {!notStarted && (
-          <div className="flex-grow flex flex-col justify-between">
-            <Timer
-              start={start}
-              reset={reset}
-              timeInCentiseconds={timeInCentiseconds}
-              setTimeInCentiseconds={setTimeInCentiseconds}
-            />
+
+        <div className="flex flex-col items-center">
+          <Timer
+            start={doingFlashcard}
+            reset={reset}
+            timeInCentiseconds={timeInCentiseconds}
+            setTimeInCentiseconds={setTimeInCentiseconds}
+          />
+          {(doingFlashcard || isAttacking) && (
             <div className="text-2xl">
               Time Elapsed: {formatTime(timeInCentiseconds)}
             </div>
-
-            {questionSet == null && <div>Loading...</div>}
-            {!!questionSet && questionSet.length === 0 && (
-              <div>None questions?</div>
-            )}
-            {!!questionSet && questionSet.length !== 0 && (
-              <AutoFlashcard
-                questionSet={questionSet}
-                onQuestionAnswer={(lastQuestionRight: boolean) => {}}
-                currentQuestionIdx={0}
-                resultTimeSecs={10}
-              />
-            )}
-            <div className="flex justify-end mb-10 mr-40 pb-10">
-              <button
-                onClick={() => setStart(!start)}
-                className={`py-2 px-4 rounded transition-colors mx-2 ${start ? "bg-red-500 hover:bg-red-600" : "bg-blue-500 hover:bg-blue-600"} text-white`}
-              >
-                {start ? "Stop" : "Start"}
-              </button>
-              <button
-                onClick={handleReset}
-                className="bg-yellow-500 text-white py-2 px-4 rounded hover:bg-yellow-600 transition-colors mx-2"
-              >
-                Reset
-              </button>
-            </div>
-          </div>
+          )}
+        </div>
+        {doingFlashcard && questionSet && questionSet !== "never" && (
+          <AutoFlashcard
+            questionSet={questionSet}
+            onQuestionAnswer={(lastQuestionRight: boolean) => {
+              setCurrentQuestionIdx((last) => {
+                if (last + 1 === NUMBER_OF_QUESTIONS) {
+                  setDoingFlashcard(false);
+                  setIsAttacking(true);
+                }
+                return Math.min(NUMBER_OF_QUESTIONS - 1, last + 1);
+              });
+            }}
+            currentQuestionIdx={currentQuestionIdx}
+            resultTimeSecs={0.5}
+          />
         )}
       </div>
     </div>
   );
-};
-
-export default GamePage;
+}
