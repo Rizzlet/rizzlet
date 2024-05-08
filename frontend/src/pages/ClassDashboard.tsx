a4890872eb4f1ce34d984749cbcf10bef27f43ad
nrojo1124 <naomi.ouat.1124@gmail.com>
2024-05-08 10:13:08 -0700

    updated achievements

--- frontend/src/pages/ClassDashboard.tsx
+++ frontend/src/pages/ClassDashboard.tsx
@@ -4,6 +4,7 @@ import {
   PlayIcon,
   QuestionMarkCircleIcon,
   RectangleStackIcon,
+  StarIcon,
 } from "@heroicons/react/20/solid";
 import React, { useEffect, useState } from "react";
 import { useParams, useSearchParams } from "react-router-dom";
@@ -13,6 +14,7 @@ import FlashcardField from "./AnswerQuestion";
 import QuestionOverview from "./QuestionOverview";
 import QuestionSubmission from "./FormSubmitQuestions";
 import DamageDealer from "../components/game/damageDealer";
+import Achievements from "../components/Achievements";
 
 interface UserStats {
   user: string;
@@ -112,6 +114,11 @@ const ClassDashboard: React.FC = () => {
                 <QuestionMarkCircleIcon className="h-4 w-4"></QuestionMarkCircleIcon>
                 Submit Questions
               </button>
+              <button className="flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900" onClick={(e) => handleLinkClick("achievements")}
+              >
+                <StarIcon className="h-4 w-4"></StarIcon>
+                Achievements
+              </button>
             </nav>
           </div>
         </div>
@@ -126,6 +133,7 @@ const ClassDashboard: React.FC = () => {
           {selectedLink === "flashcards" && <FlashcardField />}
           {selectedLink === "questions" && <QuestionOverview />}
           {selectedLink === "submit" && <QuestionSubmission />}
+          {selectedLink === "achievements" && <Achievements />}
         </div>
       </div>
     </div>
