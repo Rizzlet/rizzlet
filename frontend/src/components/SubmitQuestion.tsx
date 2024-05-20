c78bbd388dc22db1225bf59acb1abe077f09e40e
SanTran113 <stran113@calpoly.edu>
2024-05-20 13:56:17 -0700

    fix esline

--- frontend/src/components/SubmitQuestion.tsx
+++ frontend/src/components/SubmitQuestion.tsx
@@ -1,5 +1,4 @@
-import React, { ChangeEvent, ReactElement, useEffect, useState } from "react";
-import axios from "axios";
+import React, { ChangeEvent} from "react";
 import { MultipleChoiceAnswer } from "../pages/FormSubmitQuestions";
 
 export function Title() {
