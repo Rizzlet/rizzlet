3523d8f33599d7367a02b7559d0882312b35ed3d
Dylan Starink <dylan@starink.com>
2024-05-22 13:21:31 -0700

    Fix eslint

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/auth/AuthContext";
-import axios from "axios";
 
 const links = [
   { to: "/", text: "Home" },
