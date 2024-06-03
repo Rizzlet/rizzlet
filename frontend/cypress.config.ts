8ae75daab54882e0f4bea741744cf5ccb3867fea
Kevin <kev.yuan01@gmail.com>
2024-06-03 12:46:29 -0700

    Added dotenv to frontend and cypress

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "cypress";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
   e2e: {
-    baseUrl: "",
+    baseUrl: "http://127.0.0.1:8000",
+    supportFile: false,
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+    chromeWebSecurity: false,
+  },
+
+  env: {
+    googleClientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    googleAuthCode: process.env.AUTH_CODE,
+    googleRefreshToken: process.env.REFRESH_TOKEN,
   },
 });
