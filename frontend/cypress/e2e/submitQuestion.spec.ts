a44114c270d0822dc1f5e50063798c4d87d3ad56
Kevin <kev.yuan01@gmail.com>
2024-06-03 23:52:50 -0700

    finished ui e2e testing

--- frontend/cypress/e2e/submitQuestion.spec.ts
+++ frontend/cypress/e2e/submitQuestion.spec.ts
@@ -1,5 +1,5 @@
 describe("GIVEN a user has a question they want to submit", () => {
-  it("WHEN the user is on the submit question tab in the dashboard", () => {
+  beforeEach(() => {
     cy.request({
       method: "POST",
       url: "/api/auth/google/refresh",
@@ -9,25 +9,37 @@ describe("GIVEN a user has a question they want to submit", () => {
     }).then((response) => {
       const { id } = response.body;
       localStorage.setItem("authUserId", id);
-      // localStorage.setItem("token", response.header("X-"));
+      localStorage.setItem("token", response.headers["x-token-set"] as string);
+    });
+  });
 
-      // cy.intercept("GET", "/api/**", {
-      //   headers: { Authorization: `Bearer ${xtoken}` },
-      // }).as("addAuth");
+  it("WHEN the user is on the submit question tab in the dashboard", () => {});
 
-      // cy.visit("http://127.0.0.1:3000/", {
-      //   headers: {
-      //     authorization: `Bearer ${xtoken}`,
-      //   },
-      // });
+  it("THEN the user should be able to fill out the question and clear the form", () => {
+    cy.visit("http://127.0.0.1:3000");
+    cy.get("ul > li > a").eq(1).click();
+    // make sure csc309 class is there and the first class
+    cy.get("div[class=m-5] > a").click();
+    cy.get("button").eq(8).click();
 
-      // cy.request({
-      //   method: "GET",
-      //   url: "/api/user/streak",
-      //   headers: { Authorization: `Bearer ${xtoken}` },
-      // }).then((response) => {
-      //   console.log(response.body);
-      // });
+    cy.get("select").should("be.visible").select("Multiple Choice");
+
+    //fills in question
+    cy.get("input[id=question]").type("Which one is the right answer?");
+
+    // gets the first checkbox
+    cy.get("input[type=checkbox]").eq(0).check();
+
+    // fills in answer field
+
+    cy.get("input[id=answer0]").type("This is the right answer");
+    cy.get("input[id=answer1]").type("This is the wrong answer");
+    cy.get("input[id=answer2]").type("This is definitely the wrong answer");
+    cy.get("input[id=answer3]").type("This is not the right answer");
+    cy.intercept("POST", "/api/question").as("postQuestion");
+    cy.get("button[type=submit]").click();
+    cy.wait("@postQuestion").then((res) => {
+      cy.get("input[id=question]").should("have.value", "");
     });
   });
 });
